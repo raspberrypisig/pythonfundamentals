2264e59f9400bdeacba6047448a7d16eca091fa9
mohan <raspberrypisig@gmail.com>
2016-04-10 19:00:50 +1000

    Lesson 1 uploaded

---
+++ Lesson 01 - Data Types.py
@@ -0,0 +1,36 @@
+#Let's learn Python
+
+#The perennial Hello World
+print("Hello World")
+
+#Some data types available in Python
+
+#--Primitive data types
+print("hello double quotes") #string
+print('hello single quotes') #also a string
+print('''line one
+line two
+line three''') #multiline string
+print(4) #integer
+print(4.5) #float
+print(True) #boolean
+
+#--Collection data types
+print([1,2,3]) #list
+print((1,2,3)) #tuple
+print({1,2,3}) #set
+print({"convenor": "Mohan", "co-convener":"Rob"}) #dictionary
+
+#More complicated investigations into data types
+'''
+#Everything in python is an object ie. python is an object-oriented 
+#language
+print(type("hello"))
+print(type(4)) 
+print(type(4.5))
+print(type(True))
+print(type( [1,2,3]                                    )) 
+print(type( (1,2,3)                                    )) 
+print(type( {1,2,3}                                    ))
+print(type( {"convenor": "Mohan", "co-convener":"Rob"} ))
+'''
