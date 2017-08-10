70b9a5bc901e77aa56af18e8a3c083f6d6751e36
raspberrypisig <raspberrypisig@gmail.com>
2017-08-10 17:41:00 +1000

    Create Snippet 02 - Screen Scraping headless.py

---
+++ Snippet 02 - Screen Scraping headless.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# Do the following on the command line (uncomment)
+# wget -c https://github.com/fg2it/phantomjs-on-raspberry/releases/download/v2.1.1-wheezy-jessie-armv6/phantomjs_2.1.1_armhf.deb
+# sudo dpkg -i phantomjs_2.1.1_armhf.deb
+# rm -f dpkg -i phantomjs_2.1.1_armhf.deb
+# sudo pip install selenium
+
+from selenium import webdriver
+import json
+
+driver = webdriver.PhantomJS()
+driver.get('https://raw.githubusercontent.com/pklaus/python-spelling-alphabet/master/alphabets/NATO.json
+')
+print("----- START SOURCE---")
+print(driver.page_source)
+print("------END SOURCE----")
+justjson = driver.find_element_by_tag_name('body').text
+jsondata = json.loads(justjson)
+print("\n\n--- NATO Phonetic for letter B----")
+print(jsondata['letters']['B'])
+driver.close()
